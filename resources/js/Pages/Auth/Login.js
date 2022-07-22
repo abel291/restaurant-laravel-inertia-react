02dd16485588362ffb66cdd1288f7b876ad53398
Abel Morales <abel291m@gmail.com>
2022-07-22 12:51:26 -0500

    fix email

--- resources/js/Pages/Auth/Login.js
+++ resources/js/Pages/Auth/Login.js
@@ -101,7 +101,7 @@ export default function Login({ status, canResetPassword }) {
                     </p>
                 </div>
 				<div className="text-left text-sm mt-4 font-medium text-gray-300">
-					<span className="block">admin: user@user2.com</span>
+					<span className="block">admin: user@user.com</span>
 					<span className="block">invitado: user2@user.com</span>
 					<span className="block">contraseña : password</span>
 				</div>
